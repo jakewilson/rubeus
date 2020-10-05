5217ae8dca00a08faf6150127a6f23c3f61bb89b
Jake Wilson <jakewilsonfl@gmail.com>
2020-10-04 21:22:05 -0700

    Remove unneeded exception from crypto.

--- inc/crypto.hpp
+++ inc/crypto.hpp
@@ -1,21 +1,10 @@
 #pragma once
 
 #include <string>
-#include <exception>
 
 namespace crypto
 {
-    int derive_key(std::string, char *key);
+    int derive_key(std::string, char *);
     constexpr int output_len = 16;
-
-    struct crypto_exception : public std::exception
-    {
-        crypto_exception(const char *msg) : m_msg(msg) {}
-        const char * what() const throw ()
-        {
-            return m_msg;
-        }
-        const char *m_msg;
-    };
 };
 
